9a2caae708bc44f3492ce65868d7a09e8a3449d7
Hemil78 <zanattahemil@gmail.com>
2021-09-29 21:59:33 +0200

    digital

--- src/components/Digital.vue
+++ src/components/Digital.vue
@@ -1,7 +1,45 @@
 <template>
-    <div>
-        <h2>Digital</h2>
+
+    <div class="icons-descript">
+        <ul>
+          <li>
+            <a href="#">
+              <img src="@/assets/images/buy-comics-digital-comics.png" alt="">
+              <span>DIGITAL COMICS</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="@/assets/images/buy-comics-merchandise.png" alt="">
+              <span>DIGITAL COMICS</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="@/assets/images/buy-comics-subscriptions.png" alt="">
+              <span>DIGITAL COMICS</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="@/assets/images/buy-comics-shop-locator.png" alt="">
+              <span>DIGITAL COMICS</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="@/assets/images/buy-dc-power-visa.svg" alt="">
+              <span>DIGITAL COMICS</span>
+            </a>
+          </li>
+
+        </ul>
     </div>
+
 </template>
 
 <script>
@@ -16,7 +54,27 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/assets/style/variables.scss";
-  h2{
-    color: rgb(0, 121, 235);
+
+.icons-descript{
+  width: 100%;
+  ul{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    padding: 55px 0;
+    li{
+      a{
+        display: inline-block;
+        text-decoration: none;
+        color: #fff;
+        img{
+          width: 50px;
+          vertical-align: middle;
+          margin-right: 15px;
+        }
+      }
+    }
   }
+}
 </style>
